4caeb267bdff8daa4ddb607a936f5739a9c6dbec
Lucas Lassinger <58485966+lucaslassinger@users.noreply.github.com>
2020-04-24 18:20:23 -0400

    Update passwordLoop.py

--- passwordLoop.py
+++ passwordLoop.py
@@ -1,19 +1,6 @@
 ADMINPASSWORD = '0123456789'
 
 while True:
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-    print('Who are you?')
-    name = input()
-    if name == 'Lucas':
-        print ('Hello, Lucas. What is the password? (It is Gaelic.)')
-        password = input()
-        while password != 'Brogan':
-            print('Wrong password, please try again.')
-            password = input()
-=======
->>>>>>> Stashed changes
     menuSelection = input('Select 1 for admin setup, 2 for login, or 3 to quit:')
     if menuSelection == '1':
         enteredPassword = input('Please enter the admin password:')
@@ -21,8 +8,6 @@ while True:
             setupEntry = input('Welcome to admin setup. Please select 1 to add an account, select 2 to delete an account, select 3 to quit')
             ### Enter if loop for this menu
         elif enteredPassword == 'QUIT':
-<<<<<<< Updated upstream
-=======
             break
         else:
             enteredPassword = input('Incorrect password, please enter the admin password or type QUIT to exit')
@@ -37,30 +22,29 @@ while True:
                 while password != 'Brogan':
                     print('Wrong password, please try again.')
                     password = input()
+                while password == 'Brogan':
+                    print('Access granted.')
+                    break
                 break
             elif name == 'Cody':
-                print ('Hello, Cody. What is the password? (You liked her so you must have put a ring on her.)')
-                password = input()
-                if password == 'Michala':
+                password = input('Hello, Cody. What is the password? (You ringed, ranged, runged her finger.)')
+                while password != 'Michala':
+                    print('Wrong password, please try again.')
+                    password = input()
+                while password == 'Michala':
+                    print('Access granted.')
                     break
+                break
             else:
-                ####Build a bail out after too many rejections
-                continue
-        print('Access granted.')
+                name = input('Name not in system, please enter a satisfactory name or type QUIT to exit.')
+                if name == 'QUIT':
+                    break
+                else:
+                    continue
         break
     elif menuSelection == '3':
         print('System still armed, please restart to disarm system')
->>>>>>> Stashed changes
         break
-    elif name == 'Cody':
-        print ('Hello, Cody. What is the password? (You liked her so you must have put a ring on her.)')
-        password = input()
-        if password == 'Michala':
->>>>>>> Stashed changes
-            break
-        else:
-            enteredPassword = input('Incorrect password, please enter the admin password or type QUIT to exit')
-            continue
     elif menuSelection == '2':
         while True:
             ### Design to check values from a library
